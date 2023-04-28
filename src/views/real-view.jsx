491ddc1dc2ee04b6f3e433ee35adaf444ff20da1
unknown <partharora557@gmail.com>
2023-04-29 01:39:53 +0530

    donwwe

--- src/views/real-view.jsx
+++ src/views/real-view.jsx
@@ -3,6 +3,23 @@ import CanvasContainer from "../components/three";
 import IndexLayout from "../layouts";
 import Keynote from "./Keynote";
 
+const Loader = () => {
+  React.useEffect(() => {
+    console.log("loading");
+    return () => {
+      console.log("loading done");
+    };
+  });
+
+  return (
+    <div className="w-[100%] fixed top-0 left-0 h-[100vh] bg-[#21363d] flex items-center justify-center" style={{
+      zIndex:999999999999999
+    }}>
+      <img src="/logo-1.png" alt="" />
+    </div>
+  );
+};
+
 const RealView = () => {
   return (
     <IndexLayout>
@@ -11,7 +28,7 @@ const RealView = () => {
         className="w-[100%] h-[100%] absolute select-none "
         style={{ zIndex: 10 }}
       >
-        <Suspense fallback={null}>
+        <Suspense fallback={<Loader />}>
           <CanvasContainer />
         </Suspense>
       </div>
