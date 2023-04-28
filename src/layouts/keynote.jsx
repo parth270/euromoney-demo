b12ee539c63dc0bced5ff7e38b7c4f5c853dae1f
unknown <partharora557@gmail.com>
2023-04-28 23:41:38 +0530

    done

--- src/layouts/keynote.jsx
+++ src/layouts/keynote.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setCurrId, setId } from "../services/model";
 import { Power4 } from "gsap";
@@ -7,6 +7,7 @@ import Model1 from "../components/model/keynote/model-1";
 import Model2 from "../components/model/keynote/model-2";
 import Model3 from "../components/model/keynote/model-3";
 import Model4 from "../components/model/keynote/model-4";
+import AudioPlayer from "react-h5-audio-player";
 
 const poss = [
   {
@@ -27,13 +28,129 @@ const poss = [
   },
 ];
 
+const Aud = () => {
+  const ref = useRef();
+  const [src, setSrc] = useState("/eng.mp3");
+  const [srcId, setSrcId] = useState(0);
+  const opt = ["eng", "jap", "portugese"];
+  React.useEffect(() => {
+    console.log(ref.current);
+    if (ref.current) {
+      const thatDiv = ref.current.container.current;
+      const first = thatDiv.children[1];
+      console.log(first);
+      first.className = "flex w-[100%]";
+      first.children[0].className =
+        "font-medium text-[12px] text-[#fff] flex w-[70px] justify-between absolute w-[161px] items-center";
+      first.children[1].className =
+        "flex w-[160px] justify-center absolute px-[50px]";
+      first.children[1].children[0].style.display = "none";
+      first.children[1].children[2].style.display = "none";
+      first.children[1].children[1].className = "scale-150";
+      first.children[1].children[1].children[0].className = "svg-white";
+      first.children[1].children[1].children[1].className = "svg-white";
+      first.children[1].children[1].children[2].className = "svg-white";
+    }
+  });
+
+  const [clicked, setClicked] = useState(false);
+  const [hover, setHover] = useState(false);
+  React.useEffect(() => {
+    const onPointerDown = () => {
+      if (!hover) {
+        setClicked(false);
+      }
+    };
+    document.addEventListener("pointerdown", onPointerDown, false);
+    return () => {
+      document.removeEventListener("pointerdown", onPointerDown, false);
+    };
+  });
+  return (
+    <div className="w-[340px] flex relative items-center justify-between">
+      <AudioPlayer
+        ref={ref}
+        className="aud-check"
+        src={src}
+        listenInterval={10}
+        onPlay={(e) => console.log("onPlay")}
+      />
+      <div
+        onClick={() => {
+          setClicked(true);
+        }}
+        onMouseOver={() => {
+          setHover(true);
+        }}
+        onMouseLeave={() => {
+          setHover(false);
+        }}
+        style={{
+          zIndex: 9999999999999,
+        }}
+        className="w-[140px] cursor-pointer relative h-[34px] relative translate-y-[8px] rounded-sm bg-[#00000017] flex items-center justify-center font-medium text-[14px] text-[#fff]  right-0 "
+      >
+        <p className="cursor-pointer uppercase translate-x-[-3px] text-[14px] tracking-wide">
+          {opt[srcId]}
+        </p>
+        <img
+          src="/down.svg"
+          className={` ${
+            clicked ? "rotate-180" : ""
+          } absolute right-[13px] w-[10px] translate-y-[1px]`}
+          alt=""
+        />
+        {clicked && (
+          <div
+            className="w-[100%] min-h-[34px] py-[5px] px-[5px] bg-[#222] rounded-sm absolute top-[38px]"
+            style={{
+              zIndex: 1000000000000,
+            }}
+          >
+            <div
+              onClick={() => {
+                setSrcId(0);
+                setSrc("/eng.mp3");
+              }}
+              className="w-[100%] h-[34px] bg-[#333] cursor-pointer uppercase text-[12px] tracking-wide rounded-sm mb-[4px] flex items-center justify-center "
+            >
+              eng
+            </div>
+            <div
+              onClick={() => {
+                setSrcId(1);
+                setSrc("/jap.mp3");
+              }}
+              className="w-[100%] h-[34px] bg-[#333] cursor-pointer uppercase text-[12px] tracking-wide rounded-sm mb-[4px] flex items-center justify-center "
+            >
+              jap
+            </div>
+            <div
+              onClick={() => {
+                setSrcId(2);
+                setSrc("/portu.mp3");
+              }}
+              className="w-[100%] h-[34px] bg-[#333] cursor-pointer uppercase text-[12px] tracking-wide rounded-sm flex items-center justify-center "
+            >
+              portugese
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
 const Navbar = () => {
+  const state = useSelector((state) => state.model);
+
   return (
     <div
-      className="w-[100%] h-[80px] bg-[#21363D] px-[40px] flex items-center absolute"
-      style={{ zIndex: 100 }}
+      className="w-[100%] h-[80px] bg-[#21363D] px-[40px] flex items-center absolute justify-between"
+      style={{ zIndex: 1000000000000 }}
     >
       <img src="/logo-1.png" className="w-[180px]" alt="" />
+      {state.currId === 2 && <Aud />}
     </div>
   );
 };
